0cf3e04807644c30dbf53d0415199b4400050ce5
Pedro Miguel Sanchez <pedro_weba@hotmail.com>
2018-11-01 16:36:41 +0100

    Añadidos permisos y errores

--- app/src/main/java/tfg/appdemobanco/Main2Activity.java
+++ app/src/main/java/tfg/appdemobanco/Main2Activity.java
@@ -1,17 +1,32 @@
 package tfg.appdemobanco;
 
+import android.Manifest;
+import android.app.AppOpsManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.PowerManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static android.app.AppOpsManager.MODE_ALLOWED;
+import static android.app.AppOpsManager.OPSTR_GET_USAGE_STATS;
+import static android.os.Process.myUid;
+
 public class Main2Activity extends AppCompatActivity {
     Button b1;
     Button b2;
     private TextView error;
+    double res=Utils.leerValidacion();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +35,7 @@ public class Main2Activity extends AppCompatActivity {
         b2=(Button)findViewById(R.id.button3);
         error=(TextView) findViewById(R.id.textView);
         final double res=Utils.leerValidacion();
+        checkForPermission(this);
         if(res<0.35){
             error.setVisibility(View.VISIBLE);
             b1.setVisibility(View.INVISIBLE);
@@ -55,7 +71,7 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     protected void onResume(){
         super.onResume();
-        final double res=Utils.leerValidacion();
+        res=Utils.leerValidacion();
         if(res<0.35){
             error.setVisibility(View.VISIBLE);
             b1.setVisibility(View.INVISIBLE);
@@ -65,5 +81,14 @@ public class Main2Activity extends AppCompatActivity {
             b1.setVisibility(View.VISIBLE);
             b2.setVisibility(View.VISIBLE);
         }
+    }
+    private void checkForPermission(Context context) {
+        boolean hasPermissionWrite = (ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+        String[] permissions;
+
+        //Permisos estadisticas de uso y escritura en memoria
+        permissions = new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if(!hasPermissionWrite)
+            ActivityCompat.requestPermissions(this,permissions,0);
     }
 }
